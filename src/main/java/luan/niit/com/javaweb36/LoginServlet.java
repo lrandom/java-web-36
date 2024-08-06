e0cdd4711cb8cc3c417603f95557cc5dca40e69e
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-08-06 20:58:39 +0700

    get data using post

---
+++ src/main/java/luan/niit/com/javaweb36/LoginServlet.java
@@ -0,0 +1,21 @@
+package luan.niit.com.javaweb36;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "loginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+    }
+}
